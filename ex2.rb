e7266ee77b300d3ab4ee438214b35d253aa40edc
estermer <eric.stermer@gmail.com>
2016-10-21 17:59:00 -0400

    example lesson 2 complete

---
+++ ex2.rb
@@ -0,0 +1,10 @@
+# A commment, this is so you can read your program later.
+# Anything after the # is ignored by ruby.
+# I can still type ctrl / on a line and it will comment out the entire line in Atom
+
+puts "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "diable" or comment out a piece of code:
+# puts "This wont run."
+
+puts "This will run." 
