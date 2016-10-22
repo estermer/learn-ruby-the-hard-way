27017976a8f7394202c09d3596346339366a7105
estermer <eric.stermer@gmail.com>
2016-10-22 17:54:05 -0400

    completed exercise 13

---
+++ ex13.rb
@@ -0,0 +1,6 @@
+# These arguements will be taken in when the program is run
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
