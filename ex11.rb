23ab314a32f8844648e85be7cf624844ef114f7d
estermer <eric.stermer@gmail.com>
2016-10-22 17:44:50 -0400

    Added comments to exercise 11

--- ex11.rb
+++ ex11.rb
@@ -1,4 +1,5 @@
 # gets.chomp will take an input from the user and set it to the variable
+# simple gets.chomp is a simple string response from the user
 print "How old are you? "
 age = gets.chomp
 print "How tall are you? "
@@ -7,3 +8,5 @@ print "How much do you weight? "
 weight = gets.chomp
 
 puts "So, you're #{age} years old, #{height} inches tall and #{weight}lbs heavy."
+
+# there are other ways to get input in other variable types from users
