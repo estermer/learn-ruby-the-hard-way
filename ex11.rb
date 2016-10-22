d3f5a7072e8f68d988fdda501c8736feb3cad779
estermer <eric.stermer@gmail.com>
2016-10-22 17:43:15 -0400

    completed exercise 11

---
+++ ex11.rb
@@ -0,0 +1,9 @@
+# gets.chomp will take an input from the user and set it to the variable
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weight? "
+weight = gets.chomp
+
+puts "So, you're #{age} years old, #{height} inches tall and #{weight}lbs heavy."
