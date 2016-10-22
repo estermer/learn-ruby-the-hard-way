fe573454e2f065777193bf2ea2762ed66735109a
estermer <eric.stermer@gmail.com>
2016-10-22 17:00:44 -0400

    comppleted exercise 9

---
+++ ex9.rb
@@ -0,0 +1,17 @@
+days = "Mon Tues Wed Thurs Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\Jul\nAug\nSep\nOct\nNov\nDec"
+# \n creates a new line in the same string
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+
+# triple double quote marks allow you to create a string on mulitple lines
+# almost like a block quote
+puts """
+Theres something going on here.
+With the three doible-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+# each new line is a new line printed out
