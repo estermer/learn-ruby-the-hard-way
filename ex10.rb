222e32559c4120bca1eefc3eec5e7dadaa13f421
estermer <eric.stermer@gmail.com>
2016-10-22 17:30:46 -0400

    completed exercise 10

---
+++ ex10.rb
@@ -0,0 +1,18 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+
+# \\ will give a \ in the text
+backslash_cat = "I'm \\ a \\ cat."
+
+# \t is a tab
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
