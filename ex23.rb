a485a65631e04fc06947b4da13b8eb6c80510533
estermer <eric.stermer@gmail.com>
2016-10-22 23:17:34 -0400

    completed exercise 23

---
+++ ex23.rb
@@ -0,0 +1,10 @@
+# Read through source code or just any code found on GitHub
+# to familiarize my self with how Ruby is written out there
+
+# Code I Read
+
+# https://github.com/airbnb/ruby
+# https://github.com/training-mode/ruby/blob/master/intro/14_objects/rpg.rb
+# https://github.com/TwoFactorAuth/ruby/blob/master/app/controllers/two_factor_auth/registrations_controller.rb
+# https://github.com/ryanb/ruby-warrior
+# https://github.com/al3xandr3/ruby/blob/master/sesame.rb
