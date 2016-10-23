3caf71b4abd8e72b3f64910b478d31b4e8c143a0
estermer <eric.stermer@gmail.com>
2016-10-23 13:56:23 -0400

    completed exercise 30

---
+++ ex30.rb
@@ -0,0 +1,35 @@
+# Else if statements
+
+
+# Ruby checks the if statement and if it false then it checcks the next statements
+# in the else if statement chain
+
+
+people = 30
+cars = 40
+trucks = 15
+
+
+if cars > people
+  puts "We should take the cars."
+elsif cars < people
+  puts "we should not take the cars."
+else
+  puts "We can't decide."
+end
+
+if trucks > cars
+  puts "That's too many trucks."
+elsif trucks < cars
+  puts "Maybe we could take the trucks."
+else
+  puts "We still cannot decide."
+end
+
+if people > trucks
+  puts "Alright, let's just take the trucks."
+else
+  puts "Fine, let's stay home then."
+end
+
+# it is elsif not else if or elseif, else has the second e off elsif
