e4ea31e0ad609b8db0d6f0997433de06c1745779
estermer <eric.stermer@gmail.com>
2016-10-23 12:50:19 -0400

    completed exercise 29

---
+++ ex29.rb
@@ -0,0 +1,41 @@
+# What-if's
+
+
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+# if statements need end keywords to close the statements
+
+if people > cats
+  puts "Not many cats! the world is saved!"
+end
+
+if people < dogs
+  puts "The world is drooled on!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+# you can arecreate a variable adding or removing with the += or -= or /= or *=
+dogs += 5
+
+if people >= dogs
+  puts "People are greater than or equal to dogs."
+end
+
+if people <= dogs
+  puts "People are less than or equal to dogs."
+end
+
+if people == dogs
+  puts "People are dogs."
+end
+
+# the if stantment runs the embeded code when the statement is true
+# if the statement is false it will not run the embeded code and move on
