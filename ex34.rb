6c6a8aa308bbcf40f959a0e9b5369af7e0eaf4ff
estermer <eric.stermer@gmail.com>
2016-10-23 14:30:40 -0400

    completed exercise 34

---
+++ ex34.rb
@@ -0,0 +1,28 @@
+# Accessing elements of an array
+
+# arrays start with and index of 0 and go up to array.length-1
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#the animal at 1 is 'ruby'
+puts animals[1]
+
+# The third animal is 'peacock'
+puts animals[2]
+
+#The first animal is 'bear'
+puts animals[0]
+
+# the animal at 3 is 'kangaroo'
+puts animals[3]
+
+#The fifth animal is 'whale'
+puts animals[4]
+
+#The animal at 2 is 'peacock'
+puts animals[2]
+
+#The sixth animal is 'platypus'
+puts animals[animals.length-1]
+
+#The animal at 4 is 'whale'
+puts animals[4]
