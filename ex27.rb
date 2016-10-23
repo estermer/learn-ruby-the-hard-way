dfd416c9dcdf1ab8ebba75569e7d716d0098db2c
estermer <eric.stermer@gmail.com>
2016-10-23 00:04:55 -0400

    completed exercise 27

---
+++ ex27.rb
@@ -0,0 +1,11 @@
+# Logic characters
+
+# &&  (and) double ampersand
+# || (or) double pipes
+# ! (not) exclamation point
+# != (not equal) exclamation point equals
+# == (equals) double equals
+# >= (greater than or equal)
+# <= (less than or equal)
+# true
+# false
