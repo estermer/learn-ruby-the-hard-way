51b8926f2187aae5a36b3e2133ee6bba249c25be
estermer <eric.stermer@gmail.com>
2016-10-22 23:04:10 -0400

    commpleted exercise 22

---
+++ ex22.rb
@@ -0,0 +1,17 @@
+# this is just practice what I have learned and writing about it
+
+# the # is a way in which you can comment out lines in the code
+# the #{} allows for code to be read within a String
+# "" the double qutes are stings which can have expressions in them
+# '' the single quotes are simply just text and there is no expression wirting allowed
+# def begins a function while end closes it
+# functions can take in parameters
+# lines do not need a ; semicolon to end a lines
+# variables and defined with a variable name the = and the value
+# variables can be expressions, strings, text, integers, float numbers, etc
+# you can pass arguements into a file from the terminal with ARGV
+# puts will print information to the terminal and then create a new line
+# print will output information to the terminal but not create a new line
+# there are expressions in Strings that can create new lines, tabs, lashes, etc using the \ backkslash
+# you can do math with symbols such as +, -, *, /, %
+# functions can return data with the return keyword to input data into a variable after a algorithm is applied
